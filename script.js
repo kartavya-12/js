b5a8bd30c9704dee43be52eb56dd7b197a1ca500
kartavya-12 <kartavya03784@gmail.com>
2026-02-02 10:00:45 +0530

    2-02-26 10am

---
+++ script.js
@@ -0,0 +1,84 @@
+let name = "kartavya";
+let age = 18;
+
+
+let obj = {
+    name : "x",
+    phone:97777,
+    address:"xyz"
+}
+
+// console.log(obj)
+
+// console.log(document)
+
+// const para=document.querySelector('p')
+// para.textcontent= "this is now updated"
+
+
+// const para=document.getElementById('para')
+// para.textcontent="this is now updated"
+// para.style.color="green"
+// console.log(para)
+// const para=document.getElementById('para')
+
+
+// const para=document.getElementsByClassName('para')
+// para[0].textContent="this is now updated"
+// para[1].style.color="green"
+// console.log(para)
+
+// const para=document.querySelector('.para')
+// para.textcontent="this is now updated"
+// para.style.color="green"
+// console.log(para)
+
+// debugger
+// console.log(a)
+
+// var a= 78
+// let b=934
+
+// console.log(a)
+// console.log(b)
+
+// function print(){
+//     let  c=45
+//     console.log(c)
+//     console.log("inside func")
+// }
+
+// print()
+
+
+// let total = 100;
+
+// function calculate(){
+//     console.log(total)
+//     var total= 100
+// }
+// calculate()
+
+
+
+// ------------------------------xxxxxxxxxxxxx-----------------------------------------------------
+
+// setTimeout(() => {
+//     console.log("2 sec over")
+// }, 2000);
+// console.log("i didn't wait")
+
+
+
+// function print(){    //callback fn
+//     console.log("hello students")
+
+// }
+
+// function greet(num){
+//     console.log("welcome")
+//     num()
+// }
+
+// greet(print)
+
