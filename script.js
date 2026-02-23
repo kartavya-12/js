e30334aa17377249bc740c89db36174d4f472298
kartavya-12 <kartavya03784@gmail.com>
2026-02-23 10:47:05 +0530

    updated code

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let name = "kartavya";
-let age = 18;
+// let name = "kartavya";
+// let age = 18;
 
 
-let obj = {
-    name : "x",
-    phone:97777,
-    address:"xyz"
-}
+// let obj = {
+//     name : "x",
+//     phone:97777,
+//     address:"xyz"
+// }
 
 // console.log(obj)
 
@@ -198,24 +198,161 @@ let obj = {
 
 
 
-const p = new Promise(function(resolve,reject){
-    setTimeout(() => {
-        let done =true;
-        if(done){
-            resolve({name:"alex",age:34})
-        }else{
-            reject("network issue")
+// const p = new Promise(function(resolve,reject){
+//     setTimeout(() => {
+//         let done =true;
+//         if(done){
+//             resolve({name:"alex",age:34})
+//         }else{
+//             reject("network issue")
+//         }
+//     }, 5000)
+// });
+
+// //console.log(p)
+
+// p.then ((data) =>{
+//     console.log("resolved",data)
+// }).catch((err)=>{
+//     console.log("rejected",err)
+// }).finally(()=>{
+//     console.log("finally block")
+// })
+
+// console.log("first line")
+// setTimeout(() => {
+//     console.log("inside timeout")
+// }, 0);
+// const p= new Promise((resolve,reject)=>{
+//     resolve()
+// })
+
+// p.then (()=>{
+//     console.log("inside promise")
+// }).catch(()=>{
+//     console.log("inside catch")
+// })
+
+// const p2 = new Promise((resolve, reject )=>{
+//     resolve()
+// })
+// p2.then (()=>{
+//     console.log("inside promise")
+// }).catch(()=>{
+//     console.log("inside catch")
+// })
+
+// const p3 = new Promise((resolve, reject )=>{
+// //     resolve()
+// // })
+// // p3.then (()=>{
+// //     console.log("inside promise")
+// // }).catch(()=>{
+// //     console.log("inside catch")
+// // })
+
+// // console.log("last line")
+
+
+
+// async function sendData() {
+//     try{
+//         const response= await fetch ('https://dummyjson.com/products/add',{
+//             method:'post',
+//             headers: {'content-Type'; 'application/json' },
+//             body: JSON.stringify({
+//                 title: "macbook"
+//                 description:"macbook pro",
+//                 price:100000,
+//                 discountPercentage: 5,
+//                 rating:4.5,
+//                 stock:5,
+//                 brand:"apple"
+//
+//             })
+//
+//         })
+//         const data =await response.json()
+//         console.log(data)
+//     }catch(error){
+//         console.log("data not found")
+
+//     }
+// }
+// sendData()
+
+let obj={
+    title:"mac",
+    description:"mac pro",
+}
+// localStorage.setItem("obj",JSON.stringify(obj))
+// localStorage.setItem("name","kartavya")
+// localStorage.setItem("age",19)
+// console.log(localStorage.getItem("name"))
+// console.log(localStorage.getItem("age"))
+// console.log(JSON.parse(localStorage.getItem("obj")))
+
+// localStorage.removeItem("age")
+
+
+
+// sessionStorage.setItem("obj",JSON.stringify(obj))
+// sessionStorage.setItem("name","kartavya")
+// sessionStorage.setItem("age", 19)
+// console.log(sessionStorage.getItem("name"))
+// console.log(sessionStorage.getItem("age"))
+// console.log(JSON.parse(sessionStorage.getItem("obj")))
+
+// document.cookie="name= kartavya; expires = 21 feb 2026 12:00:00 UTC"
+// document.cookie="age=19; expires = 21 feb 2026 12:00:00 UTC"
+
+// console.log(document.cookie)
+
+// async function sample() {
+//     await fetch()
+// }
+// sample()
+
+function generator(){
+    yield 1
+    yield 2
+    yield 3
+     
+    for (let index =1; index<4; index++){
+        yield index
+    }
+
+    let i=1;
+    while(true){
+        yield i
+        i
+    }
+}
+
+const gen = generator()
+
+console.log(gen)
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+
+
+
+
+function add(a){
+    return function(b){
+        return function(c){
+            return a+b+c
         }
-    }, 5000)
-});
+    }
+}
 
-//console.log(p)
 
-p.then ((data) =>{
-    console.log("resolved",data)
-}).catch((err)=>{
-    console.log("rejected",err)
-}).finally(()=>{
-    console.log("finally block")
-})
+console.log((1)(2)(3))
+
+const first= add(1)
+const second=first(2)
+const third=second(3)
+console.log(third)
 
